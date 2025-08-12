df589e94b551cf155c983f32a769f0969a05b910
tioRaffa <rafaelmuniz200@gmail.com>
2025-08-12 10:35:51 -0300

    implementando função principal para envio de mensagens utilizando SuperBase e ZAPI

---
+++ main.py
@@ -0,0 +1,36 @@
+from superbase_service import SuperBaseService
+from zapi_service import ZAPIService
+from decouple import UndefinedValueError
+import logging
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+
+def main():
+    logging.info('Iniciando processo de envio de mensagem..')
+
+    try:
+        superbase_service = SuperBaseService()
+        zapi_service = ZAPIService()
+
+        contacts = superbase_service.get_contacts()
+
+        if contacts:
+            for contac in contacts:
+                name = contac.get('nome_contato')
+                phone = contac.get('numero_telefone')
+
+                if name and phone:
+                    zapi_service.send_greeting_message(contact_name=name, phone_number=phone)
+                else:
+                    logging.warning(f'Registro de contato invalido.. pulando: {contac}')
+    
+    except Exception as e:
+        logging.critical(f'Ocorreu um erro inesperado: {e}')
+    except UndefinedValueError:
+        logging.critical('Execução interrompida...')
+
+    logging.info('Processo Finalizado')
+
+if __name__ == '__main__':
+    main()
